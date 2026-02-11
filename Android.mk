c1da40ff22e19c44620698396f66e620a650f73e
S4r4-77 <saraa.j8888@gmail.com>
2026-02-11 20:13:27 +0500

    Refactor Android.mk for jakarta target device only
    Remove native packages fixup logic for non-jakarta devices.

--- Android.mk
+++ Android.mk
@@ -6,20 +6,10 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jakarta, $(TARGET_DEVICE)),)
+# Only run if the target device is jakarta
+ifeq ($(TARGET_DEVICE),jakarta)
+
+# Include all sub-makefiles (like recovery/Android.mk)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-NATIVE_PACKAGES_FIXUP := $(TARGET_OUT_VENDOR)/etc/native_packages.xml
-$(NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
-	@echo "Move vendor native_packages.bin to native_packages.xml"
-	$(hide) mv $(TARGET_OUT_VENDOR)/etc/native_packages.bin $@
-
-SYSTEM_NATIVE_PACKAGES_FIXUP := $(PRODUCT_OUT)/system/etc/native_packages.xml
-$(SYSTEM_NATIVE_PACKAGES_FIXUP): $(PRODUCT_OUT)/system/etc/native_packages.bin
-	@echo "Move system native_packages.bin to native_packages.xml"
-	$(hide) mv $(PRODUCT_OUT)/system/etc/native_packages.bin $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(NATIVE_PACKAGES_FIXUP) $(SYSTEM_NATIVE_PACKAGES_FIXUP)
 
 endif
